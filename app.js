7a46eb55795018308face0e59579648a0e983a65
Victor Pinho <69443505+victorfpinho@users.noreply.github.com>
2025-07-01 19:49:06 -0300

    Finalização do projeto

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+    }
+
     let sorteados = [];
     let numero;
 
@@ -20,12 +30,36 @@ function sortear() {
     }
 
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+    alterarStatusBotao();
 
 }
 
 //-----------------------------------------------------------------------------FUNÇÃO OBTER NÚMERO ALEATÓRIO
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+//----------------------------------------------------------------------------ALTERAR STATUS BOTÃO REINICIAR
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+//--------------------------------------------------------------------------------FUNÇÃO BOTÃO REINICIAR
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarStatusBotao();
 
 }
