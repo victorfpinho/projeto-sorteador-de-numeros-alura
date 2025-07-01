4d3c87092ed5fb40fa6613309e1546906b56fb2b
Victor Pinho <69443505+victorfpinho@users.noreply.github.com>
2025-07-01 13:36:18 -0300

    Criando e atualizando comentários

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//-------------------------------------------------------------------------------------------FUNÇÃO SORTEAR
 function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
@@ -6,9 +7,11 @@ function sortear() {
     let sorteados = [];
     let numero;
 
+    //--------------------------------------------------------------------Criando lista de números sorteados
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
 
+        //---------------------------------------------------------------------Verificando números repetidos
         while (sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate);
         }
@@ -21,6 +24,7 @@ function sortear() {
 
 }
 
+//-----------------------------------------------------------------------------FUNÇÃO OBTER NÚMERO ALEATÓRIO
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 
